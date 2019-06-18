11a3e4b3ca8bd1b8b1362218e207885d11558849
ninja-wizard <terbot4@gmail.com>
2019-06-19 00:53:53 +0300

    day 1

---
+++ script.js
@@ -0,0 +1,34 @@
+function update_slider(reset=false)
+{
+    slider_element.style.display = "none";
+    if (slider_element.nextElementSibling == null) 
+    { 
+        slider_element = document.getElementsByClassName("se1");
+        slider_element = slider_element[0];
+    }
+    else slider_element = slider_element.nextElementSibling;
+    slider_element.style.display = "block";
+    if (reset) 
+    {
+        clearInterval(slider_interval);
+        slider_interval = setInterval(update_slider, 4000);
+    }
+}
+var slider_element = document.getElementsByClassName("se1");
+slider_element = slider_element[0];
+var slider_interval = setInterval(update_slider, 4000);
+
+function dropdown(item)
+{
+    item = item.nextElementSibling;
+    item.style.display = "block";
+    item.style.left = event.clientX;    
+}
+window.onclick = function (event) {
+    if (!event.target.matches('h2')) {
+        var dropdowns = document.getElementsByClassName("mm_dropdown");
+        Array.from(dropdowns).forEach(menu => {
+            menu.style.display = "none";
+        });
+    }
+} 
